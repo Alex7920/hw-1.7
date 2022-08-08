a4ccf4a1c5964e80f7c304e9534e43a412a619dd
Александр <kuzya-79@yandex.ru>
2022-08-08 20:12:14 +0300

    Сделано дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.nio.file.FileSystemNotFoundException;
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Ivan";
+        String surname = "Ivanov";
+        String patronymic = "Ivanovich";
+        String fullName = surname +" " + name + " " + patronymic;
+        System. out. println("фио сотрудника - " + fullName );
+
+        System. out. println("Данные фио сотрудника для заполнения отчёта  - " + fullName.toUpperCase() );
+
+        fullName = "Иванов Семён Семёнович";
+        String replaceFullName = fullName.replace("ё","e");
+        System.out.println("Данные ФИО сотрудника - " +replaceFullName );
     }
 }
